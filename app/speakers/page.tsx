137ba68b4ebb4c48a10a0845811c180693d5e4aa
Aayush Rajagopalan <61064137+Aayush-Rajagopalan@users.noreply.github.com>
2024-12-09 16:18:13 +0300

    .

--- app/speakers/page.tsx
+++ app/speakers/page.tsx
@@ -84,7 +84,7 @@ export default function About() {
                 </div>
               </div>
 
-              {/*              <div className="container px-4 md:px-6">
+                            <div className="container px-4 md:px-6">
                 <div className="mx-auto grid max-w-5xl items-center gap-6 py-12 lg:grid-cols-2 lg:gap-10">
                   <div>
                     <div className="relative isolate">
@@ -101,7 +101,7 @@ export default function About() {
                         />
                       </div>
                       <Image
-                        src="https://cdn.aayus.me/tedx/speaker1"
+                        src="https://cdn.aayus.me/tedx/speaker2.jpg"
                         alt="product preview"
                         width={1920}
                         height={1080}
@@ -126,21 +126,10 @@ export default function About() {
                   <div className="flex flex-col justify-center space-y-4">
                     <div className="grid gap-1">
                       <h3 className="text-5xl pb-4 font-bold">
-                        Dr. Avantika Tomar
+                        Kavach Khanna
                       </h3>
                       <p className="text-neutral-500 dark:text-neutral-400">
-                        Dr. Avantika Tomar is a Partner in EY-Parthenon’s
-                        education sector practice, and is based in Bangalore. As
-                        a strategy and transactions consultant, she advises
-                        education operators, multi-national corporates,
-                        governments, foundations and private equity firms across
-                        India, MENA and SE Asia. Her areas of expertise within
-                        the education sector are K12, test prep and higher
-                        education (with a focus on TNE). Avantika holds a PhD in
-                        strategy and HR from IIM Calcutta, and is currently a
-                        Visiting Professor at various business schools (IIMC, U,
-                        T, BITSoM, Masters Union etc.) in India. She has
-                        previously taught at UNSW and UTS in Sydney, Australia.
+As a dedicated Student Mentor, Educator, and Content Creator, he has mentored over 6,000 students, guiding them toward making informed career choices and achieving their goals. With a passion for empowering individuals to unlock their full potential, he offers personalized advice and actionable insights. His work as a content creator has further amplified his impact, with his YouTube channel garnering over 15 million views, reflecting the trust and value his audience places in his content. He remains committed to inspiring and transforming lives through mentorship and engaging educational initiatives.
                       </p>
                     </div>
                   </div>
@@ -151,21 +140,10 @@ export default function About() {
                   <div className="flex flex-col justify-center space-y-4">
                     <div className="grid gap-1">
                       <h3 className="text-5xl pb-4 font-bold">
-                        Dr. Avantika Tomar
+                          Rithwick Varier
                       </h3>
                       <p className="text-neutral-500 dark:text-neutral-400">
-                        Dr. Avantika Tomar is a Partner in EY-Parthenon’s
-                        education sector practice, and is based in Bangalore. As
-                        a strategy and transactions consultant, she advises
-                        education operators, multi-national corporates,
-                        governments, foundations and private equity firms across
-                        India, MENA and SE Asia. Her areas of expertise within
-                        the education sector are K12, test prep and higher
-                        education (with a focus on TNE). Avantika holds a PhD in
-                        strategy and HR from IIM Calcutta, and is currently a
-                        Visiting Professor at various business schools (IIMC, U,
-                        T, BITSoM, Masters Union etc.) in India. She has
-                        previously taught at UNSW and UTS in Sydney, Australia.
+Rithwick Varier is a seasoned entrepreneur and technology consultant with over a decade of experience in healthcare and digital innovation. An alumnus of DPS MIS and the University of Illinois at Urbana-Champaign, he spent five years in Tampa, Florida, implementing the Affordable Care Act through technology consulting and systems optimization. He then dedicated another five years to startups, leading the development of cutting-edge, design-driven technology to make self-funded healthcare more accessible. As the founder of A-Gaido, a USA-based startup, he empowers businesses to embrace transformative digital solutions. A mentor, Toastmaster, and animal welfare advocate, Rithwick is an inspiring leader and engaging speaker.
                       </p>
                     </div>
                   </div>
@@ -184,7 +162,7 @@ export default function About() {
                         />
                       </div>
                       <Image
-                        src="https://cdn.aayus.me/uploads/20231104_140232.jpg"
+                        src="https://cdn.aayus.me/tedx/speaker3.jpg"
                         alt="product preview"
                         width={1920}
                         height={1080}
@@ -226,7 +204,7 @@ export default function About() {
                         />
                       </div>
                       <Image
-                        src="https://cdn.aayus.me/uploads/20231104_140232.jpg"
+                        src="https://cdn.aayus.me/tedx/speaker4.jpg"
                         alt="product preview"
                         width={1920}
                         height={1080}
@@ -251,21 +229,11 @@ export default function About() {
                   <div className="flex flex-col justify-center space-y-4">
                     <div className="grid gap-1">
                       <h3 className="text-5xl pb-4 font-bold">
-                        Dr. Avantika Tomar
-                      </h3>
+                        Pratham Mittal
+</h3>
                       <p className="text-neutral-500 dark:text-neutral-400">
-                        Dr. Avantika Tomar is a Partner in EY-Parthenon’s
-                        education sector practice, and is based in Bangalore. As
-                        a strategy and transactions consultant, she advises
-                        education operators, multi-national corporates,
-                        governments, foundations and private equity firms across
-                        India, MENA and SE Asia. Her areas of expertise within
-                        the education sector are K12, test prep and higher
-                        education (with a focus on TNE). Avantika holds a PhD in
-                        strategy and HR from IIM Calcutta, and is currently a
-                        Visiting Professor at various business schools (IIMC, U,
-                        T, BITSoM, Masters Union etc.) in India. She has
-                        previously taught at UNSW and UTS in Sydney, Australia.
+Pratham Mittal is a dynamic entrepreneur and educator, currently serving as the Founder of Tetr and Masters' Union. Masters' Union is a unique higher education institution where courses are taught by industry leaders from top companies like Morgan Stanley, Tata, StanChart, Google, and more. The institution has successfully placed students in prestigious firms such as McKinsey, BCG, Bain, and Softbank.
+Before founding Masters' Union, Pratham co-founded Outgrow.co, an interactive content platform used by top agencies and Fortune 500 companies. His innovative approach to education and business has made significant impacts in both fields, demonstrating his commitment to bridging the gap between academia and industry.
                       </p>
                     </div>
                   </div>
@@ -276,21 +244,10 @@ export default function About() {
                   <div className="flex flex-col justify-center space-y-4">
                     <div className="grid gap-1">
                       <h3 className="text-5xl pb-4 font-bold">
-                        Dr. Avantika Tomar
+                        Aditya Jain
                       </h3>
                       <p className="text-neutral-500 dark:text-neutral-400">
-                        Dr. Avantika Tomar is a Partner in EY-Parthenon’s
-                        education sector practice, and is based in Bangalore. As
-                        a strategy and transactions consultant, she advises
-                        education operators, multi-national corporates,
-                        governments, foundations and private equity firms across
-                        India, MENA and SE Asia. Her areas of expertise within
-                        the education sector are K12, test prep and higher
-                        education (with a focus on TNE). Avantika holds a PhD in
-                        strategy and HR from IIM Calcutta, and is currently a
-                        Visiting Professor at various business schools (IIMC, U,
-                        T, BITSoM, Masters Union etc.) in India. She has
-                        previously taught at UNSW and UTS in Sydney, Australia.
+                    Aditya Jain is a dynamic student leader, author, and aspiring economist with a passion for global economic frameworks and policymaking. A four-time presenter at the Qatar Leadership Conference (QLC) and Deputy Secretary General at MSMUN and THIMUN, he has showcased exceptional leadership and public speaking skills. During the pandemic, Aditya authored the science fiction novel Miles Away from Home, reflecting his creativity and resilience. As Head Boy, he led impactful initiatives to foster collaboration and excellence, while excelling in sports by representing his school in cricket and tennis. With multiple ‘All-Rounder’ and ‘Subject Topper’ awards to his credit, Aditya brings his inspiring journey of leadership, perseverance, and innovation to the TEDx stage.
                       </p>
                     </div>
                   </div>
@@ -309,7 +266,7 @@ export default function About() {
                         />
                       </div>
                       <Image
-                        src="https://cdn.aayus.me/tedx/speaker1"
+                        src="https://cdn.aayus.me/tedx/speaker6.jpg"
                         alt="product preview"
                         width={1920}
                         height={1080}
@@ -332,7 +289,7 @@ export default function About() {
                     </div>
                   </div>
                 </div>
-              </div>*/}
+              </div>
             </section>
           </div>
         </div>
